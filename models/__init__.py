65a5bb21e45c3cca4a88bb92853b0668884c109a
Kyalo460 <Kyalo460@gmail.com>
2023-12-21 18:44:47 +0300

    undid previous change

--- models/__init__.py
+++ models/__init__.py
@@ -5,12 +5,11 @@ import os
 
 HBNB_TYPE_STORAGE = os.environ.get('HBNB_TYPE_STORAGE')
 
-if HBNB_TYPE_STORAGE == 'fs':
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-    storage.reload()
-
-else:
+if HBNB_TYPE_STORAGE == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
     storage.reload()

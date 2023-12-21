171b25f3f27638d783dadd518b2b89525552b2f3
Kyalo460 <Kyalo460@gmail.com>
2023-12-21 18:42:54 +0300

    changed order of if else

--- models/__init__.py
+++ models/__init__.py
@@ -5,11 +5,12 @@ import os
 
 HBNB_TYPE_STORAGE = os.environ.get('HBNB_TYPE_STORAGE')
 
-if HBNB_TYPE_STORAGE == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-    storage.reload()
-else:
+if HBNB_TYPE_STORAGE == 'fs':
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+    storage.reload()
+
+else:
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
     storage.reload()

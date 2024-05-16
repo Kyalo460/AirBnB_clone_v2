2cf8f931971ae406645b0ed953dd501e3da2b2df
Kyalo460 <Kyalo460@gmail.com>
2024-05-16 16:07:31 +0300

    Added comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
  #!/usr/bin/python3
 from flask import Flask
+"""Starts a flask application."""
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Serves the get request to the url /"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":

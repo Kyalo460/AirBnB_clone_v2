c2c585456bf77b836f18bb88c469d69f6ca0efb8
Kyalo460 <Kyalo460@gmail.com>
2024-05-17 16:50:39 +0300

    different solution

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -42,14 +42,11 @@ def python(text):
     return f'Python {text}'
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Displays number if it's an integer
     """
-    if n.isdigit():
-        return f'{n} is a number'
-    else:
-        abort(404)
+    return f'{n} is a number'
 
 
 if __name__ == "__main__":
